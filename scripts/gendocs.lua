de514eb28bb422e96c2f1a2e5e93ab0c3466ad8f
lpoto <luka.potochnik@gmail.com>
2022-11-27 23:40:05 +0100

    Add github action for generating docs

---
+++ scripts/gendocs.lua
@@ -0,0 +1,27 @@
+local docgen = require "docgen"
+
+local docs = {}
+
+docs.test = function()
+  local input_files = {
+    "./lua/actions/init.lua",
+    "./lua/actions/model/user_config.lua",
+    "./lua/actions/model/log_config.lua",
+    "./lua/actions/model/mappings_config.lua",
+    "./lua/actions/model/action.lua",
+  }
+  local output_file = "./doc/actions.txt"
+  local output_file_handle = io.open(output_file, "w")
+  for _, input_file in ipairs(input_files) do
+    docgen.write(input_file, output_file_handle)
+  end
+  if output_file_handle ~= nil then
+    output_file_handle:write " vim:tw=78:ts=8:ft=help:norl:\n"
+    output_file_handle:close()
+  end
+  vim.cmd [[checktime]]
+end
+
+docs.test()
+
+return docs
